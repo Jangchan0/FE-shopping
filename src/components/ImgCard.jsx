59189ae803bc2f6a8b5d2bf7248fc68bac3c0776
jakesungjaekim <codejakecarry@gmail.com>
2023-02-14 17:02:31 +0900

    Add: ImgCard-UI 완료

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const ImgCard = () => {
-  return <div>ImgCard</div>;
-};
+const ImgCard = styled.img`
+  width: ${({ width }) => (width ? width : '100px')};
+  height: ${({ height }) => (height ? height : '100px')};
+
+  padding: ${({ padding }) => (padding ? padding : '0px')};
+  margin: ${({ margin }) => (margin ? margin : '0px')};
+
+  border: ${({ border }) => (border ? border : 'none')};
+  border-radius: ${({ borderRadius }) => (borderRadius ? borderRadius : '0px')};
+
+  object-fit: ${({ objectFit }) => (objectFit ? objectFit : 'fill')};
+  object-position: ${({ objectPosition }) =>
+    objectPosition ? objectPosition : 0};
+`;
 
 export default ImgCard;
