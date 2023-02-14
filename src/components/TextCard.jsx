d117b34b4b1b6171e0a4a62fc35818f4420787b8
Jangchan0 <jchy16@gmail.com>
2023-02-14 17:56:44 +0900

    ADD: TextCard UI 완성

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const TextCard = () => {
-  return <div>TextCard</div>;
-};
+const TextCard = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection }) =>
+    flexDirection ? flexDirection : 'column'};
+  justify-content: ${({ justifyContent }) =>
+    justifyContent ? justifyContent : 'flex-start'};
+  align-items: ${({ alignItems }) => (alignItems ? alignItems : 'flex-start')};
+
+  width: ${({ width }) => (width ? width : '514px')};
+  height: ${({ height }) => (height ? height : '400px')};
+
+  border: ${({ border }) => (border ? border : 'none')};
+  border-radius: ${({ borderRadius }) => (borderRadius ? borderRadius : '0')};
+
+  margin: ${({ margin }) => (margin ? margin : '0')};
+  padding: ${({ padding }) => (padding ? padding : '0')};
+
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
+  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '400')};
+
+  color: ${({ color }) => (color ? color : 'black')};
+  background-color: ${({ bgColor }) => (bgColor ? bgColor : 'none')};
+`;
 
 export default TextCard;
