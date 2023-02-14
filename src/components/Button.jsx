60e6f081f0ce284717d3f5b3d9f19ab7a9383f2a
Jangchan0 <jchy16@gmail.com>
2023-02-14 17:19:28 +0900

    ADD: 버튼 UI 완성

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-  return <div>Button</div>;
-};
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  width: ${({ width }) => (width ? width : '665px')};
+  height: ${({ height }) => (height ? height : '100px')};
+
+  border: ${({ border }) => (border ? border : 'none')};
+  border-radius: ${({ borderRadius }) =>
+    borderRadius ? borderRadius : 'none'};
+
+  margin: ${({ margin }) => (margin ? margin : '0')};
+  padding: ${({ padding }) => (padding ? padding : '0')};
+
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
+  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '700')};
+
+  color: ${({ color }) => (color ? color : 'black')};
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor ? backgroundColor : 'transparent'};
+`;
 
 export default Button;
