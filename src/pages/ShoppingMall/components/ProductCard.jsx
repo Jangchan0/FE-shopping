b6adf5922071dfd1eac7af648cb21f70517a74c4
jakesungjaekim <codejakecarry@gmail.com>
2023-02-15 12:23:41 +0900

    Add: ShoppingMall 기능완료..

--- src/pages/ShoppingMall/components/ProductCard.jsx
+++ src/pages/ShoppingMall/components/ProductCard.jsx
@@ -3,12 +3,20 @@ import CardContainer from '../../../components/CardContainer';
 import ImgCard from '../../../components/ImgCard';
 import TextCard from '../../../components/TextCard';
 
-const ProductCard = ({ imgUrl, description, price, discount }) => {
+const ProductCard = ({
+  imgUrl,
+  description,
+  priceBefore,
+  priceAfter,
+  discount,
+  date,
+  isbn,
+}) => {
   return (
     <CardContainer
       flexDirection="column"
       width="514px"
-      height="447px"
+      height="500px"
       borderRadius="15px"
       margin="auto"
     >
@@ -17,13 +25,17 @@ const ProductCard = ({ imgUrl, description, price, discount }) => {
         alt="img1"
         width="100%"
         height="306px"
-        objectFit="contain"
+        objectFit="cover"
+        objectPosition="center"
       />
       <TextCard padding="2rem 0.5rem">
         <p className="description"> {description} </p>
-        <p className="price">
-          {price} <span className="discount"> {discount}</span>
+        <p className="price-after">
+          {priceAfter} <span className="price-before">{priceBefore}</span>
+          <span className="discount"> {discount}</span>
         </p>
+        <p className="date">등록일: {date}</p>
+        <p className="isbn">ISBN: {isbn}</p>
       </TextCard>
     </CardContainer>
   );
