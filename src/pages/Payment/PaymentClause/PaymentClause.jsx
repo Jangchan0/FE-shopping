75d9dcc2e0e825605894687c15585edefe23619d
Jangchan0 <jchy16@gmail.com>
2023-02-16 12:23:11 +0900

    ADD: Clause-UI 제작

--- src/pages/Payment/PaymentClause/PaymentClause.jsx
+++ src/pages/Payment/PaymentClause/PaymentClause.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Togle from './components/Togle';
+import TextCard from '../../../components/TextCard';
 
 const PaymentClause = () => {
-  return <div>PaymentClause</div>;
+  const [clause, setClause] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/Clause.json')
+      .then(result => result.json())
+      .then(data => setClause(data));
+  }, []);
+
+  return (
+    <ClauseContainer>
+      {clause.length > 1 && (
+        <>
+          <TextCard
+            margin="5rem auto"
+            width="65rem"
+            height="7rem"
+            fontSize="1.1rem"
+          >
+            <span className="topTitle">{clause[1].ClauseTitle[0].value}</span>
+          </TextCard>
+          <div>
+            {clause[0].ClauseContents.map((value, i) => {
+              return <Togle key={i} clause={value} />;
+            })}
+          </div>
+        </>
+      )}
+    </ClauseContainer>
+  );
 };
 
 export default PaymentClause;
+
+const ClauseContainer = styled.div`
+  width: 100vw;
+  .topTitle {
+    line-height: 140%;
+  }
+`;
